36a6f8d98816ec99302104382fe15cebd7600475
0xkid-root <prohackers698@gmail.com>
2026-03-25 17:28:41 +0530

    create-food page done  with intergation

--- frontend/src/pages/general/Home.jsx
+++ frontend/src/pages/general/Home.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>Home
+      <a href="/create-food">Create Food</a><br />
+      <a href="/food-list">Food List</a><br />
+      <a href="/foodpartner-list">Food Partner List</a><br />
+    </div>
   )
 }
 export default Home
